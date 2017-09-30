afd808ec9efdc2f6a298c285afb079dbf6c34970
Lamar, Frank <frankie_lamar1@homedepot.com>
2017-09-30 18:49:07 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -18,8 +18,9 @@ mv iiab /opt/iiab
 mv admin-console /opt/iiab
 mv menu /opt/iiab
 mv factory /opt/iiab
+mv local_var.yml /opt iiab
 
 export DEBIAN_FRONTEND=noninteractive
 
-wget http://wget http://download.iiab.io/6.4/rpi/local_vars.yml
+
 
