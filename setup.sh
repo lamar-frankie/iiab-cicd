99919cf431b23b6384f26920fe1dfe3a4438b510
Lamar, Frank <frankie_lamar1@homedepot.com>
2017-09-30 18:12:11 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash -x
 
+#updating os
+apt update
+apt dist-upgrade -y
+apt clean -y
+
+#installing dependencies
+
+apt install -y git
+apt install -y wget
+apt install -y ansible
+
+#setup environment
+mkdir -p /opt/iiab
+
+mv iiab /opt/iiab
+mv iiab-admin-console /opt/iiab
+mv iiab-menu /opt/iiab
+mv iiab-factory /opt/iiab
+
 export DEBIAN_FRONTEND=noninteractive
 
 wget http://wget http://download.iiab.io/6.4/rpi/local_vars.yml
