984f862cffcf9fcf5f7aa7c9e4b66435cf6efd02
Lamar, Frank <frankie_lamar1@homedepot.com>
2017-09-30 20:13:17 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -x
 
 #setup environment
-mkdir -p /iiab
-
-mv iiab /iiab
-mv admin-console /iiab
-mv menu /iiab
-mv factory /iiab
-mv cicd-repo/local_var.yml /iiab/iiab/vars/
+apt update
+apt upgrade -y
+apt clean -y
 
 export DEBIAN_FRONTEND=noninteractive
 
